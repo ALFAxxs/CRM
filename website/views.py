205e9bbbeabe597f55eb07bb6f9b107d6590a602
ALFAxxs <bahodirerkinov111@gmail.com>
2025-05-12 20:23:55 +0500

    updated 8:23

--- website/views.py
+++ website/views.py
@@ -1,4 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 def home(request):
-    return render(request, 'home.html',  {})
+    #check to see if logging in
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in!")
+            return redirect('home')
+        else:
+            messages.success(request, "There was an error Logging in , Please try again!")
+    return render(request, 'home.html', {})
+
+
+def register_user(request):
+    return render(request, 'registration.html', {})
+
+
+def login_user(request):
+    pass
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out!")
+    return redirect('home')
